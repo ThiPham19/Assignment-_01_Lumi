dce314207b63d75edaaea156f1a3059618c1d1a1
ThiPham19 <phamhongthi19@gmail.com>
2023-09-07 11:06:21 +0700

    init project

---
+++ Debug/Src/main.d
@@ -0,0 +1,27 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/melody.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/buzzer.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/eventbutton.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/melody.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/buzzer.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/eventbutton.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+C:/Users/Dell/STM32CubeIDE/workspace_1.12.1/library_copy/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
